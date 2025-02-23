4e5078e49d83747e084193ae8ef50ca1b0384baa
AbhishekBudur <abhishekchotu0@gmail.com>
2025-02-23 15:18:43 +0530

    Initial commit

---
+++ src/main/java/BasePackage/BaseTest.java
@@ -0,0 +1,13 @@
+package BasePackage;
+
+public class BaseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BaseTest obj = new BaseTest();
+		obj.setup();
+	}
+	public void setup() {
+		System.out.println("Set up method started");
+	}
+}
